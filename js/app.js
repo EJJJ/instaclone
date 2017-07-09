a2b1fb0cd01ee2792535d48f0e33e5953e69c616
EJJJ <ejestar1211@naver.com>
2017-07-09 17:11:38 +0900

    17:11

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
+    
+    var $heart = $('.heart'),
+        $addComment = $('.photo__add-comment');
 
-	var $heart = $('.heart');
+    $heart.click(function(){
+        var likes = $(this).parent().parent()
+                    .children('.photo__likes')
+                    .children('.photo__likes-number'),
+            likesNumber = parseInt(likes.html()),
+            newValue
+        if($(this).hasClass('fa-heart-o')){
+            newValue = likesNumber + 1;
+        } else{
+            newValue = likesNumber - 1;
+        }
+        likes.html(newValue)
+        $(this).toggleClass('fa-heart-o fa-heart');
+    });
 
-	$heart.click(function(){
-		$(this).toggleClass('fa-heart-o fa-heart');
-	})
+    	
+	$addComment.keydown(function(event){
+		if(event.keyCode == 13){
+			var newComment = event.target.value;
+			var commentList = $(this).parent().parent().children('.photo__comments');
+			$(this).val('').blur();
+			commentList.append("<li class='photo__comment'><span class='photo__comment__author'>Bambie</span> " + newComment + "</li>");		
+		}
+	})	
 })
-
-
 
 
 
